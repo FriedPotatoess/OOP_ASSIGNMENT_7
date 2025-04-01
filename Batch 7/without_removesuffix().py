7ccacd68d30fe0fef581ac2b121e08390d9bf7b7
Jayvee <bustamantejayvee6@gmail.com>
2025-04-01 20:31:33 +0800

    Completed program 2, copied the code from removeprefix from last batch and tweaked a few things

--- Batch 7/without_removesuffix().py
+++ Batch 7/without_removesuffix().py
@@ -1,1 +1,14 @@
 #Create a program that do the same functionality without using removesuffix() function.
+
+#Fucntion for custom removesuffix
+def remove_suffix(user_input, suffix):
+    if user_input[:len(suffix)] == suffix:
+        return user_input[len(suffix):]
+    return user_input
+
+#Gets the input and the prefix that is wished to be removed
+user_input = input("Input your stuff: ")
+suffix = input("Input which suffix you wanna remove: ")
+
+#Outputs the input without the suffix
+print(remove_suffix(user_input, suffix))
