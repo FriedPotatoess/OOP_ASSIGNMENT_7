a2cf124ee8c2b7b3e3a323b8733e39e438d36bb6
Jayvee <bustamantejayvee6@gmail.com>
2025-04-01 21:32:02 +0800

    Finished program 8, using a while loop to count for the word that wanna be counted, then storing and outputting how many instance of that word is in the input

--- Batch 7/without_count().py
+++ Batch 7/without_count().py
@@ -1,2 +1,22 @@
 #Create a program that do the same functionality without using count() function.
 
+#Custom definition for the count function
+def count(user_input, word_in_search):
+    word_count = 0
+    i = 0
+    
+    while i < len(user_input):
+        i = user_input.find(word_in_search, i)
+        if i == -1:
+            break
+        word_count += 1
+        i += len(word_in_search)
+        
+    return word_count
+
+#Gets the user input and the word that the user wanna find and count
+user_input = input("Enter a sentence: ")
+word_in_search = input("Enter what word or character you wanna count: ")
+
+#Outputs the word count 
+print(f"The word '{word_in_search}' appears {count(user_input, word_in_search)} times in the sentence")
