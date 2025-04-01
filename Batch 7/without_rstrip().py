5eea62b034bcfea517a338d10b560ed2622c5f88
Jayvee <bustamantejayvee6@gmail.com>
2025-04-01 20:18:54 +0800

    Finished program one, defining a custom rstrip function,inside using an index to keep track of the position of the character, then once identified where the spaces is, is then sliced using while loop to check

--- Batch 7/without_rstrip().py
+++ Batch 7/without_rstrip().py
@@ -1,2 +1,14 @@
 #Create a program that do the same functionality without using rstrip() function.
 
+#Custom rstrip
+def rstrip(user_input):
+    index = len(user_input) - 1  
+    while index >= 0 and user_input[index] == ' ':  
+        index -= 1  
+    return user_input[:index + 1]
+
+#Takes user input
+user_input = input("Enter your sentence: ")
+
+#Prints the modified sentence
+print(f'"{rstrip(user_input)}"')
