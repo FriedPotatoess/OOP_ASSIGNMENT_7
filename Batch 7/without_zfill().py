5b43c88b79b17cb15d539a2b78d1e7eed37164e9
Jayvee <bustamantejayvee6@gmail.com>
2025-04-01 21:12:16 +0800

    Finished program 7*, fairly easy since its the same principle athe rjust, infact i just adjusted the space to zero

--- Batch 7/without_zfill().py
+++ Batch 7/without_zfill().py
@@ -1,2 +1,16 @@
 #Create a program that do the same functionality without using zfill() function.
 
+#Function for the custom zfill
+def zfill(user_input, width):
+    if len(user_input) >= width:
+        return user_input
+    else:
+        return '0' * (width - len(user_input)) + user_input
+    
+#Takes the input and the supposed width
+user_input = input("Input words or sentence: ")
+width = int(input("How long should the characters be?: "))
+
+#Prints the input with the zeroes required
+print(zfill(user_input, width))
+
